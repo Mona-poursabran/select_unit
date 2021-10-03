002d246dac713d33e5bbbae18b761fa73b72e507
mona <poursabranenglish@gmail.com>
2021-10-03 11:53:09 +0330

    menu

---
+++ menus.py
@@ -0,0 +1,40 @@
+def menu():
+    print('''
+Welcome to Maktab University
+[1] - Register
+[2] - Log in
+[3] - Exit''')
+
+def access_student():
+    print('''
+Access Menu:
+[1] - List Lessons
+[2] - Search leason
+[3] - Show total units
+[4] - Choose lesson
+[5] - Show chosen lessons
+[6] - Log out''')
+
+
+def access_edu_admin():
+        print('''
+Access Menu:
+[1] - List Lessons
+[2] - Search leason
+[3] - Show total units
+[4] - Add a new lesson
+[5] - Choose a lesson and show students 
+[6] - List students
+[7] - Search students
+[8] - Choose student and show chosen lesson
+[9] - Log out''')
+
+def access_pro():
+        print('''
+Access Menu:
+[1] - List Lessons
+[2] - Search leason
+[3] - Choose lesson 
+[4] - List chosen lesson 
+[5] - Show students list choosing the same lesson 
+[6] - Loug Out''')
